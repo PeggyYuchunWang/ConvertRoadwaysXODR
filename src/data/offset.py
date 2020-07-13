d754f07bd1d4ab5178e1d5e8d33bded2081926fc
dea-dressel <ddressel@stanford.edu>
2020-07-13 16:22:37 -0700

    docustring formatted

--- src/data/offset.py
+++ src/data/offset.py
@@ -2,9 +2,21 @@ class Offset:
     """
         An offset of the whole dataset may be applied to avoid large coordinates and
         enable inertial relocation and re-orientation of datasets.
+
         Found within the Header element.
+
+        Parameters
+        ----------
+        x : float
+            The value by which the dataset is translated for x
+        y : float 
+            The value by which the dataset is translated for y
+        z : float 
+            The value by which the dataset is translated for z
+        hdg : float
+            The value by which the dataset is rotated around the origin
     """
-    def __init__(self, x = 0, y = 0, z = 0, hdg = 0):
+    def __init__(self, x = 0, y = 0, z = 0, hdg = 0) -> None:
         self.x = float(x)
         self.y = float(y)
         self.z = float(z) #x, y, z translation
