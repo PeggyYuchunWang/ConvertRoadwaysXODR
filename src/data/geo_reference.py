656c47e2c3771df2fbf192181857daa25c80aa5e
dea-dressel <ddressel@stanford.edu>
2020-07-13 16:18:21 -0700

    docstring formatted

--- src/data/geo_reference.py
+++ src/data/geo_reference.py
@@ -1,7 +1,20 @@
 class Geo_Reference:
     """
-        Information about the geographic reference for an OpenDRIVE dataset.
+        Information about the geographic reference for an OpenDRIVE dataset. 
+        Defined by parameters describing the geodetic datum. 
+        
+        A geodetic datum is a coordinate reference system for a collection 
+        of positions that are relative to an ellipsoid model of the earth.
+        A geodetic datum is described by a projection string according to PROJ, 
+        that is, a format for the exchange of data between two coordinate systems.
+        
         Found within the Header element.
+
+        Parameters
+        ----------
+        text : str
+            The proj-string that contains all parameters that define the 
+            spatial reference system
     """
     def __init__(self, text = ""):
         self.text = text
