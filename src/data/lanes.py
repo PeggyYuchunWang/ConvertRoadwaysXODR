ed588fee9aed6380b12a39d643549beca04992c6
dea-dressel <ddressel@stanford.edu>
2020-07-16 17:38:27 -0700

    docstring formatted

--- src/data/lanes.py
+++ src/data/lanes.py
@@ -1,8 +1,22 @@
-import Lane_Section
-import Lane_Offset
-import Lane
-
 class Lanes:
-    def __init__(self):
+    """
+        All roads contain Lanes. The center lane has no width and acts as
+        the reference line for the road with a lane number of zero.
+        Lane numbering starts with 1 next to the center lane, descending in
+        negative t-direction and ascending in the positive t-direction.
+
+        Found within a Road element.
+        
+        Attributes
+        ----------
+        lane_offsets : list
+            List of Lane_Offset elements to describe the shift of the 
+            center lane away from the road reference line.
+        lane_sections : list
+            List of Lane_Section elements that each contain a fixed 
+            number of lanes.
+
+    """
+    def __init__(self) -> None:
+        self.lane_offsets = []
         self.lane_sections = []
-        self.lane_offset = 0
