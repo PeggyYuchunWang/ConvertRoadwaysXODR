cace1392dce1db821534f144b350794f3f0d4f5a
dea-dressel <ddressel@stanford.edu>
2020-07-22 15:52:16 -0700

    initial commit

---
+++ src/data/controller.py
@@ -0,0 +1,30 @@
+class Controller:
+    """
+    Provides a wrapper for the behaviour of a group of signals.
+    
+    Parameters
+    ----------   
+    id : str
+        Unique ID within database.
+    sequence : float
+        Sequence number (priority) of this controller with respect 
+        to other controllers of the same logical level.
+    name : str 
+        Name of the controller. May be chosen freely.
+
+    Attributes
+    ----------
+    attrib : dict
+         Attributes dictionary for the parameters specified above.  
+    signal_control_records : list
+        List of Controller_Signal_Control element types.
+
+    """
+    def __init__(self, id = "", sequence = "", name = "") -> None:        
+        self.attrib = {
+            "id" : str(id),
+            "sequence" : str(sequence),
+            "name" : str(name)     
+        }
+        self.signal_control_records = []
+        
---
+++ src/data/controller_signal_control.py
@@ -0,0 +1,27 @@
+class Controller_Signal_Control:
+    """
+    Provides information about a single signal controlled by 
+    corresponding controller.
+
+    Found within a Controller element.
+    
+    Parameters
+    ----------   
+    signal_id : str
+        ID of the controlled signal.
+    type : str
+        Type of control. Free text, depending on applicaiton.
+
+    Attributes
+    ----------
+    attrib : dict
+         Attributes dictionary for the parameters specified above.  
+
+
+    """
+    def __init__(self, signal_id = "", type = "") -> None:        
+        self.attrib = {
+            "signal_id" : str(signal_id),
+            "type" : str(type)  
+        }
+        
