030df6f54619aec1480c2ee33c70956eff61edf5
dea-dressel <ddressel@stanford.edu>
2020-08-14 15:22:03 -0700

    intermediate arc curve point creation functionality

--- pyxodr/utils.py
+++ pyxodr/utils.py
@@ -49,6 +49,8 @@ def createCurves(road, lane_section, curr_geos, nsamples):
             
                 # step through each lane
                 for lane in side:
+                    # print("current_x: ", current_x)
+                    # print("current_y: ", current_y)
                     tag = (road.attrib["id"], lane_section_id, lane.attrib["id"])
                     current_x, current_y, c = createLaneCurve(road,
                                                               lane,
@@ -58,7 +60,7 @@ def createCurves(road, lane_section, curr_geos, nsamples):
                                                               nsamples)
                     if c is not None:
                         if tag in curves.keys():
-                            curves[tag].curve_points.extend(c.curve_points[1:])
+                            curves[tag].curve_points.extend(c.curve_points) #[1:])
                         else:
                             curves[tag] = c
                 
@@ -111,14 +113,29 @@ def createLaneCurve(road, lane, geometry, current_x=0, current_y=0, nsamples=2):
 
         x1 = current_x
         y1 = current_y
-        r = 1/arc.attrib["curvature"]  # radius
+        print("x1: ", x1)
+        print("y1: ", y1)
+        radius = 1.0 / arc.attrib["curvature"]
+        r_squared = math.pow(radius,2)
 
         # solve for center points of circle that represents the arc
-        x0, y0 = fsolve(arc_equations, [math.pow(r,2), heading], args=(x1, y1))
+        # root = fsolve(arc_equations, [x1*.95, y1*.95], args=(x1, y1, radius, heading))
+        # print(np.isclose(arc_equations(root, x1, y1, radius, heading), [0.0, 0.0]))
+        
+        x0 = x1 - math.sqrt(r_squared / (1 - math.pow(math.tan(heading), 2)))
+        y0 = y1 - math.sqrt(r_squared - math.pow((x1-x0),2))
+        
+        print("x0, y0: ", x0 , " , ", y0 )
+        
+        # print("r^2 ", math.pow(radius,2), " = ", math.pow((x1 - x0), 2) - math.pow((y1 - y0), 2))  # set equal to radius^2
+        # print("heading ", heading, " = ", math.atan((y1-y0)/(x1-x0)))
 
         curve = populate_curve_points_arc(
             [x1 + dx, y1 + dy],
             [x0, y0],
+            heading,
+            radius,
+            geometry.attrib["length"],
             nsamples
         )
 
@@ -129,12 +146,12 @@ def createLaneCurve(road, lane, geometry, current_x=0, current_y=0, nsamples=2):
     # elif type(geometry.type) is ParamPoly3:
     #     geo_type = "polyparam3"
 
-    next_x = x1 + (math.copysign(width, float(lane.attrib["id"]) * math.sin(heading)))
-    next_y = y1 + (math.copysign(width, float(lane.attrib["id"]) * math.cos(heading)))
+    next_x = x1 + (width * math.sin(heading) * lane_sign)
+    next_y = y1 + (width * math.cos(heading) * lane_sign)
     
     return next_x, next_y, curve
 
-def populate_curve_points_line(start, end, nsamples):
+def populate_curve_points_line(start, end, nsamples=2):
         """
         Populates a Curve element with @nsamples CurvePt elements that
         describe the curve of a lane with a line geometry type.
@@ -142,7 +159,7 @@ def populate_curve_points_line(start, end, nsamples):
         Otherwise, returns a Curve element populated with CurvePt elements.
         """
         if start == end:
-            # invalid start/end points. Return empty list
+            # invalid start/end points
             return None
 
         theta = math.atan2(end[1]-start[1], end[0]-start[0])
@@ -159,30 +176,32 @@ def populate_curve_points_line(start, end, nsamples):
 
         return Curve(curve_points)
 
-def populate_curve_points_arc(start, end, curvature, nsamples=5):
+def populate_curve_points_arc(start, center, heading, radius, arc_length, nsamples=3):
         """
-        THIS IS THE EXACT SAME FUNCTION AS POPULATE_CURVE_POINTS_LINE CURRENTLY
+        
         """
-        if start == end:
-            # invalid start/end points. Return empty list
+        if start == center:
+            # invalid start/center points
             return None
 
-        theta = math.atan2(end[1]-start[1], end[0]-start[0])
-        delta = np.linalg.norm([end[0]-start[0], end[1]-start[1]]) / (nsamples-1)
+        theta_end = arc_length / radius  
+        theta = heading  
+        delta_theta = (arc_length / (nsamples-1)) / radius
 
         s = 0.0
         curve_points = [CurvePt] * nsamples
-        for i in range(1, nsamples+1):
-            t = (i-1)/(nsamples-1)
-            P_x = start[0] + (end[0]-start[0])*t
-            P_y = start[1] + (end[1]-start[1])*t
-            curve_points[i-1] = CurvePt([P_x, P_y], theta, s, 0.0)
-            s += delta
+        for i in range(0, nsamples):
+            cp_theta = theta_end - theta
+            P_x = radius * math.cos(cp_theta) + center[0]
+            P_y = radius * math.sin(cp_theta) + center[1]
+            curve_points[i] = CurvePt([P_x, P_y], cp_theta, s, 0.0)
+            s += P_x
+            theta += delta_theta
 
         return Curve(curve_points)
 
-def arc_equations(vars, x1, y1):
+def arc_equations(vars, x1, y1, radius, heading):
     x0, y0 = vars
-    eq1 = math.pow((x1 - x0),2) - math.pow((y1 - y0),2)  # set equal to radius^2
-    eq2 = math.atan((y1-y0)/(x1-x0))  # set equal to heading 
-    return [eq1, eq2]
+    eq1 = math.pow((x1-x0), 2) + math.pow((y1-y0), 2) - math.pow(radius, 2) 
+    eq2 = math.atan((y1-y0)/(x1-x0)) - heading 
+    return eq1, eq2
