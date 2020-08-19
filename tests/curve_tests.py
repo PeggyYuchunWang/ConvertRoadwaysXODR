442f74920ea87c828fbe6c6af265413aeaf0a18a
exoticDFT <exoticdft@gmail.com>
2020-08-18 22:21:26 -0700

    Updates curve tests module
    - Updates the values to check against for the first straight lane section of the curve generation test.
    - Adds the remaining right lane start point checks.

--- tests/curve_tests.py
+++ tests/curve_tests.py
@@ -23,9 +23,10 @@ class CurveTests(unittest.TestCase):
         curve_points = self.parser.curves[key].curve_points
 
         # Start position
-        self.assertAlmostEqual(curve_points[0].pos[0], -59.22845240)
-        self.assertAlmostEqual(curve_points[0].pos[1], -26.59487346)
-        self.assertAlmostEqual(curve_points[0].theta, 0.331898)
+        self.assertAlmostEqual(curve_points[0].s, 0.0)
+        self.assertAlmostEqual(curve_points[0].pos[0], -59.22773623)
+        self.assertAlmostEqual(curve_points[0].pos[1], -26.59462662)
+        self.assertAlmostEqual(curve_points[0].theta, 0.33186980)
         self.assertEqual(curve_points[0].k, 0.0)
         self.assertEqual(curve_points[0].kd, 0.0)
 
@@ -37,9 +38,10 @@ class CurveTests(unittest.TestCase):
         curve_points = self.parser.curves[key].curve_points
 
         # Start position
-        self.assertAlmostEqual(curve_points[0].pos[0], -57.97700374)
-        self.assertAlmostEqual(curve_points[0].pos[1], -30.22522818)
-        self.assertAlmostEqual(curve_points[0].theta, 0.331898)
+        self.assertAlmostEqual(curve_points[0].s, 0.0)
+        self.assertAlmostEqual(curve_points[0].pos[0], -57.97662055)
+        self.assertAlmostEqual(curve_points[0].pos[1], -30.22509623)
+        self.assertAlmostEqual(curve_points[0].theta, 0.33186980)
         self.assertEqual(curve_points[0].k, 0.0)
         self.assertEqual(curve_points[0].kd, 0.0)
 
@@ -51,9 +53,10 @@ class CurveTests(unittest.TestCase):
         curve_points = self.parser.curves[key].curve_points
 
         # Start position
+        self.assertAlmostEqual(curve_points[0].s, 0.0)
         self.assertAlmostEqual(curve_points[0].pos[0], -57.12136569)
         self.assertAlmostEqual(curve_points[0].pos[1], -32.70686256)
-        self.assertAlmostEqual(curve_points[0].theta, 0.331898)
+        self.assertAlmostEqual(curve_points[0].theta, 0.33186980)
         self.assertEqual(curve_points[0].k, 0.0)
         self.assertEqual(curve_points[0].kd, 0.0)
 
@@ -65,9 +68,40 @@ class CurveTests(unittest.TestCase):
         curve_points = self.parser.curves[key].curve_points
 
         # Start position
-        self.assertAlmostEqual(curve_points[0].pos[0], -54.85219128)
+        self.assertAlmostEqual(curve_points[0].s, 0.0)
+        self.assertAlmostEqual(curve_points[0].pos[0], -55.95821908)
         self.assertAlmostEqual(curve_points[0].pos[1], -36.08206477)
-        self.assertAlmostEqual(curve_points[0].theta, 0.331898)
+        self.assertAlmostEqual(curve_points[0].theta, 0.33186980)
+        self.assertEqual(curve_points[0].k, 0.0)
+        self.assertEqual(curve_points[0].kd, 0.0)
+
+        # End position
+
+        # Check values for road 1, first lane section and lane id of 1
+        # (The only road, first line planView geometry and first left lane)
+        key = ("1", 0, -2)
+        curve_points = self.parser.curves[key].curve_points
+
+        # Start position
+        self.assertAlmostEqual(curve_points[0].s, 0.0)
+        self.assertAlmostEqual(curve_points[0].pos[0], -55.10296422)
+        self.assertAlmostEqual(curve_points[0].pos[1], -38.56383111)
+        self.assertAlmostEqual(curve_points[0].theta, 0.33186980)
+        self.assertEqual(curve_points[0].k, 0.0)
+        self.assertEqual(curve_points[0].kd, 0.0)
+
+        # End position
+
+        # Check values for road 1, first lane section and lane id of 1
+        # (The only road, first line planView geometry and first left lane)
+        key = ("1", 0, -3)
+        curve_points = self.parser.curves[key].curve_points
+
+        # Start position
+        self.assertAlmostEqual(curve_points[0].s, 0.0)
+        self.assertAlmostEqual(curve_points[0].pos[0], -53.85184854)
+        self.assertAlmostEqual(curve_points[0].pos[1], -42.19430071)
+        self.assertAlmostEqual(curve_points[0].theta, 0.33186980)
         self.assertEqual(curve_points[0].k, 0.0)
         self.assertEqual(curve_points[0].kd, 0.0)
 
